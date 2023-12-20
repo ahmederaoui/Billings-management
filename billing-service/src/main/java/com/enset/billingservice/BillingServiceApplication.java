9c16751136f71cfaf630ee4267673155967d27a9
ahmederaoui <ahmederaoui1@gmail.com>
2023-12-20 04:27:13 +0100

    containerize services with docker

--- billing-service/src/main/java/com/enset/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/enset/billingservice/BillingServiceApplication.java
@@ -10,6 +10,7 @@ import com.enset.billingservice.repositories.ProductItemRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -17,6 +18,7 @@ import java.util.Date;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class BillingServiceApplication {
 
     public static void main(String[] args) {
--- customer-service/src/main/java/enset/com/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/enset/com/customerservice/CustomerServiceApplication.java
@@ -5,9 +5,11 @@ import enset.com.customerservice.repositores.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
